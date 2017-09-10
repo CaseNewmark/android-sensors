2ab2eb579e7712c246f309f4a4e25df0ad063a0a
Daniel Waldorf <daniel.waldorf@tomra.com>
2017-09-10 14:54:12 +0200

    initial commit

---
+++ app/src/main/java/com/woodvillage/sensortest/SensorListActivity.java
@@ -0,0 +1,87 @@
+package com.woodvillage.sensortest;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class SensorListActivity extends AppCompatActivity {
+
+    private ListView sensorListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_list);
+
+        this.sensorListView = (ListView)findViewById(R.id.listview_sensor_list);
+        SensorManager sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        SensorListAdapter<Sensor> adapter = new SensorListAdapter<Sensor>(
+                this,
+                android.R.layout.simple_list_item_1,
+                sensorList);
+        this.sensorListView.setAdapter(adapter);
+    }
+
+    private class SensorListAdapter<S> extends ArrayAdapter<Sensor> {
+
+        private List<Sensor> sensors;
+
+        public SensorListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List sensors) {
+            super(context, resource, sensors);
+
+            this.sensors = sensors;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater li = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+            View sensorListViewItem = li.inflate(R.layout.view_sensor_info, null);
+
+            Sensor sensor = this.sensors.get(position);
+            if (sensor != null) {
+                ((TextView)sensorListViewItem
+                        .findViewById(R.id.textview_sensor_name))
+                        .setText(sensor.getName());
+                ((TextView)sensorListViewItem
+                        .findViewById(R.id.textview_sensor_vendor))
+                        .setText(sensor.getVendor());
+//                ((TextView)sensorListViewItem
+//                        .findViewById(R.id.textview_sensor_version))
+//                        .setText(sensor.getVersion());
+                ((TextView)sensorListViewItem
+                        .findViewById(R.id.textview_sensor_type))
+                        .setText(sensor.getStringType());
+                ((TextView)sensorListViewItem
+                        .findViewById(R.id.textview_sensor_max_range))
+                        .setText(Float.toString(sensor.getMaximumRange()));
+                ((TextView)sensorListViewItem
+                        .findViewById(R.id.textview_sensor_resolution))
+                        .setText(Float.toString(sensor.getResolution()));
+                ((TextView)sensorListViewItem
+                        .findViewById(R.id.textview_sensor_power))
+                        .setText(Float.toString(sensor.getPower()));
+                ((TextView)sensorListViewItem
+                        .findViewById(R.id.textview_sensor_min_delay))
+                        .setText(Integer.toString(sensor.getMinDelay()));
+            }
+
+            return sensorListViewItem;
+        }
+    }
+
+}
